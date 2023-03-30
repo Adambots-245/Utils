ba5c342df7e0227e873a70447620a33a0ea592a4
Tommy R <drumming.dog101@gmail.com>
2023-03-29 21:46:20 -0400

    Ready to use

--- PathFlipper.java
+++ PathFlipper.java
@@ -1,7 +1,5 @@
-import javax.lang.model.element.Element;
-
 public class PathFlipper {
-    static final String input = "14.837493400734528,-3.0200039768061373,0.5051282994411175,0.0,true,false,12.072580603793673,-2.9801254268502593,-0.558299699382288,0.1196356498676332,true,false,9.453889156691037,-2.953539726879674,0.026585699970585353,-1.9939274977938855,true,false,";
+    static final String input = "";
     static final double width = 16.5417;
 
     public static void main (String Args[]) {
